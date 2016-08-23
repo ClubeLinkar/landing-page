7c2f939eb141d3ac1f81f7be0903af0711b901b7
Lennon Jesus <lennon.jesus@gmail.com>
2016-08-22 22:08:54 -0300

    ga localhost ignore

--- gulp/inject.js
+++ gulp/inject.js
@@ -31,9 +31,19 @@ gulp.task('inject', ['styles'], function () {
   /*  exclude: [/bootstrap\.js/, /bootstrap\.css/, /bootstrap\.css/, /foundation\.css/] */
   };
 
+  var injectAnalytcs = gulp.src(['src/assets/js/analytics.js']);
+
+  var injectAnalytcsOptions = { // This is the file that has the content that will be injected into index.html
+    starttag: '<!-- inject:analytics -->', // Here we tell the location in which we want the injection to occur
+    transform: function (filePath, file) {
+      return file.contents.toString('utf8') // Return file contents as string
+    }
+  };
+
   return gulp.src(paths.src + '/*.html')
     .pipe($.inject(injectStyles, injectOptions))
     .pipe($.inject(injectScripts, injectOptions))
+    .pipe($.inject(injectAnalytcs, injectAnalytcsOptions))
     .pipe(wiredep(wiredepOptions))
     .pipe(gulp.dest(paths.tmp + '/serve'));
 
